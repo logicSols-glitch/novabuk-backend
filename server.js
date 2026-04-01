914c0e9f4845562ca8b39815d0f6900a3009718f
Logsols <nelsonjeremiah699@gmail.com>
2026-04-01 22:31:44 +0100

    cors added

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:5500",
   "http://localhost:5501",   
-  "http://127.0.0.1:5501",     
+  "http://127.0.0.1:5501", 
+  "http://localhost:5502",   
+  "http://127.0.0.1:5502",
 ];
 
 app.use(
